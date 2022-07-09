3e8c5cf015f5fd90818e1c50da33da3d12c85621
= <=>
2022-07-09 11:07:25 +0400

    Game-project-csharp

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+
+       
+    {
+        IUserValidationService iUserValidationService;
+
+        public GamerManager(IUserValidationService iUserValidationService)
+        {
+            this.iUserValidationService = iUserValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (iUserValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Gamer registered");
+            }
+            else
+            {
+                Console.WriteLine("validation failed registered failed..(");
+            }
+
+           
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Gamer deleted");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Gamer uptaded");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { FirstName = "Jone", LastName = "Doe",IdentityNumber = 123});
+        }
+    }
+}
